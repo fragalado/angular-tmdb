18c0eff0e8ce7507c14d54db586b52aa0a56bf67
fragalado <frandanigd@gmail.com>
2025-03-18 13:28:51 +0100

    Cambios en environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiKey: 'devKey'
+    apiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMmE3MjYxMzUwZDM5MWExZGRmNDQ1OGUyMTMyNzkwZCIsIm5iZiI6MTc0MTE5MTAwOC4yMDg5OTk5LCJzdWIiOiI2N2M4Nzc2MDkwMzU1YzY0NTc2ZTVkNjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1RkkW1x4_MJK2OhThHhHVunR0shkGgoACutVCqEvxFA'
 };
