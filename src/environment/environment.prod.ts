c0ec74ed9b5072cbd499014beec3f6b54c83b1c3
fragalado <frandanigd@gmail.com>
2025-03-18 13:25:32 +0100

    Cambios en el angular.json

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiKey: 'prodKey'
+    apiKey: process.env['apiKey'] || 'prodKey'
 };
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiKey: 'devKey'
+    apiKey: process.env['apiKey'] || 'devKey'
 };
