ab124e9cedf4944a39d9306425d4984cae143a77
fragalado <frandanigd@gmail.com>
2025-03-18 13:12:43 +0100

    Environment variable

--- src/app/interceptors/add-bearer-token.interceptor.ts
+++ src/app/interceptors/add-bearer-token.interceptor.ts
@@ -1,8 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../environment/environment';
 
 export const addBearerTokenInterceptor: HttpInterceptorFn = (req, next) => {
 
-  const bearerToken = "token";
+  const bearerToken = environment.apiKey;
 
   // Añadimos el token Bearer a la peticion
   req.headers.set('Authorization', 'Bearer ' + bearerToken);
